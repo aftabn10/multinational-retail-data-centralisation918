9f59a8f3dfd690c1fc4e946827b2f81076ba52f0
Aftab Najib <aftab@mediadezines.co.uk>
2024-01-15 17:04:09 +0000

    Add the read_rds_table method into DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -3,5 +3,13 @@ from sqlalchemy import create_engine, MetaData, Table, select
 import yaml
 
 class DataExtractor:
-    pass
+    #Step 5
+    def read_rds_table(self, table_name):
+
+        query = f"SELECT * FROM {table_name}"
+        result = self.engine.execute(query)
+
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+        return df
 
