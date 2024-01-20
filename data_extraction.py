01d52172737c3bb36ee459fa3b44f032851987cd
Aftab Najib <aftab@mediadezines.co.uk>
2024-01-20 19:13:49 +0000

    Removed Calls for Functions within File and created separate main.py file

--- data_extraction.py
+++ data_extraction.py
@@ -6,20 +6,15 @@ class DataExtractor:
         self.connector = connector
 
     def read_rds_table(self, table_name):
-        # Use the list_db_tables method to get the list of tables
-        engine = self.connector.init_db_engine()
+        try:
+            # Use the init_db_engine method to create an engine
+            engine = self.connector.init_db_engine()
 
-        # Use the engine to extract the table to a pandas DataFrame
-        query = f"SELECT * FROM {table_name}"
-        df = pd.read_sql_query(query, engine)
+            # Use the engine to extract the table to a pandas DataFrame
+            query = f"SELECT * FROM {table_name}"
+            df = pd.read_sql_query(query, engine)
 
-        return df
-
-# # Call the methods
-# connector = DatabaseConnector()
-# data_extractor = DataExtractor(connector)
-# print("Tables in the database:", data_extractor.connector.list_db_tables())
-# # Test reading a non-user data table
-# df = data_extractor.read_rds_table('legacy_users')
-# print("Legacy Users:")
-# print(df.head())
+            return df
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
