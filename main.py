796d162e5e3757e1d79b2555f66b7fc872810cef
Aftab Najib <aftab@mediadezines.co.uk>
2024-01-20 19:14:30 +0000

    Create main.py file to run code

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ connector = DatabaseConnector()
 extractor = DataExtractor(connector)
 
 # Use init_db_engine method to create an engine
-engine = connector.init_db_engine()
 db_creds = connector.read_db_creds()
+engine = connector.init_db_engine()
 tables = connector.list_db_tables()
 
 # Use the read_rds_table method through the DataExtractor instance
+print("Before read_rds_table method call")
 df = extractor.read_rds_table('legacy_users')
+print("After read_rds_table method call")
 
 print("Database Credentials:", db_creds)
 print("Database Engine:", engine)
