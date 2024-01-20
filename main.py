1057fcd796cb612bdd19e782d0dd84adf0e2f21b
Aftab Najib <aftab@mediadezines.co.uk>
2024-01-20 20:21:02 +0000

    Add link for PDF, Task 4, Step 2, Tested, Working, Output printed to df

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ engine = connector.init_db_engine()
 tables = connector.list_db_tables()
 
 # Use the read_rds_table method through the DataExtractor instance
-print("Before read_rds_table method call")
-df = extractor.read_rds_table('legacy_users')
-print("After read_rds_table method call")
+# print("Before read_rds_table method call") # Task 3 Step 4 # Not Working
+# df = extractor.read_rds_table('legacy_users')
+# print("After read_rds_table method call")
+link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+pdf_df = extractor.retrieve_pdf_data(link)
 
 print("Database Credentials:", db_creds)
 print("Database Engine:", engine)
 print("Tables in the database:", tables)
-print(df.head())
+#print(df.head())
+print(pdf_df)
