de20b02fe5792e8c7b93d013d0f2a469c1d325cf
Aftab Najib <aftab@mediadezines.co.uk>
2024-02-01 22:00:11 +0000

    Fixed issue with .loc error

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class DataCleaning:
 #TASK STEP 6     
     def __init__(self):
@@ -9,14 +11,14 @@ class DataCleaning:
         # Create a copy of df
         df_filled = df.copy()
         # Fill NaN values with 0
-        df_filled = df.fillna(value=0)
+        df_filled = df_filled.fillna(value=0)
         # Drop columns with all NaN values
-        df_filled = df.dropna(axis=1, how='all')
+        df_filled = df_filled.dropna(axis=1, how='all')
         # Remove row with specific last name
-        df_filled = df[df['last_name'] != '4YSEX8AY1Z']
+        df_filled = df_filled[df_filled['last_name'] != '4YSEX8AY1Z']
         # Filter rows based on valid countries
-        df_filled = df[df['country'].isin(valid_countries)]
+        df_filled = df_filled[df_filled['country'].isin(valid_countries)]
         # Replace values in the country column
-        df_filled['country'] = df['country'].str.replace('GGB', 'GB')
+        df_filled['country'] = df_filled['country'].str.replace('GGB', 'GB')
 
         return df_filled
